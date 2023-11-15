ae79f38562ba445f497bd0c727ffbff2e1ae1fc1
Lumic-code <wisdomlumic0368@gmail.com>
2023-11-15 15:55:15 +0100

    some tests done

---
+++ TestNinja.Tests/FizzBuzzTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3Only_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5Only_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber()
+        {
+            var result = FizzBuzz.GetOutput(1);
+
+            Assert.That(result, Is.EqualTo("1"));
+
+        }
+
+        //[Test]
+        //public void GetOutput_InputIsZero_ReturnTheSameNumber()
+        //{
+        //    var result = FizzBuzz.GetOutput(0);
+
+        //    Assert.That(result, Is.EqualTo("0"));
+        //}
+
+        [Test]
+        [TestCase(-1, "-1")]
+        [TestCase(0, "0")]
+        public void GetOutput_InputIsNegative_ReturnTheSameNumber(int number, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+
+    }
+}
---
+++ TestNinja.Tests/ReservationTests.cs
@@ -0,0 +1,54 @@
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class ReservationTests
+        {
+            [Test]
+            public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+            {
+                // Arrange
+                var reservation = new Reservation();
+
+                // Act
+                var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+                // Assert
+                Assert.IsTrue(result);
+
+            }
+
+            [Test]
+            public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnsTrue()
+            {
+                // Arrange
+                var user = new User();
+                var reservation = new Reservation { MadeBy = user };
+
+
+                // Act
+                var result = reservation.CanBeCancelledBy(user);
+
+                // Assert
+                Assert.IsTrue(result);
+            }
+
+            [Test]
+            public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnsFalse()
+            {
+
+                var reservation = new Reservation { MadeBy = new User() };
+
+
+                // Act
+                var result = reservation.CanBeCancelledBy(new User());
+
+                // Assert
+                Assert.IsFalse(result);
+            }
+        }
+    }
+}
